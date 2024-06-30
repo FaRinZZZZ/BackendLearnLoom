5cf22166309f99a692d715692b5a5ee8a65958d2
mels <chanatan.melmel@gmail.com>
2024-06-30 10:39:22 +0700

    feat: update code

---
+++ app.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from Typhoon import getNodeFromPDF, getFlashCard
+from PDFExtract import pdfExtract
+
+class Item(BaseModel):
+    pdf: str
+
+class QA(BaseModel):
+    topic: str
+    n: int
+
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/ping')
+async def show():
+    return {"pong": "pong"}
+
+@app.get('/')
+async def genNode():
+    pdf_path = "temp.pdf"
+    pdf = pdfExtract(pdf_path)
+    return getNodeFromPDF(pdf)
+
+@app.post('/getNode')
+async def getNode(file: UploadFile):
+    try:
+        file_path = "temp.pdf"
+        with open(file_path, "wb") as f:
+            f.write(file.file.read())
+        print("File saved successfully")
+        pdf = pdfExtract(file_path)
+        print("sending back response")
+        return getNodeFromPDF(pdf)
+    except Exception as e:
+        print(e)
+        return {"message": e.args}
+
+@app.post('/getFlashCards')
+async def getFlashCards(qa: QA):
+    res = getFlashCard(qa.topic, qa.n)
+    return res
